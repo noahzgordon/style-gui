883a5845e1c4e0c72c89f550adf28dd9c3a75d42
Noah Zachary Gordon <tortuga.ng@gmail.com>
2017-09-04 15:29:43 -0400

    WIP

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,11 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Html exposing (Html)
+import Color exposing (..)
+import Style exposing (..)
+import Style.Color exposing (..)
+import Element exposing (..)
+import Element.Attributes exposing (..)
 
 
 -- model
@@ -31,9 +36,40 @@ update message model =
 -- view
 
 
+type Styles
+    = None
+    | Header
+    | Title
+    | MainContent
+
+
 view : Model -> Html Message
 view model =
-    text "Hello world!"
+    layout styles
+        <| column None
+            []
+            [ header, mainContent ]
+
+
+header =
+    row Header
+        [ width (percent 100), height (px 100), center ]
+        [ el Title [ center, verticalCenter ] (Element.text "Style GUI") ]
+
+
+mainContent =
+    row MainContent [ height (fill 1) ] []
+
+
+styles =
+    styleSheet
+        [ style Header
+            [ background black, Style.Color.text white ]
+        ]
+
+
+
+-- subscriptions
 
 
 subscriptions =
